60e566ef65c9dfdcc6c7bb51dd1deab18fe271f0
Eliza Hynek <eliza.hynek@gmail.com>
2023-07-26 18:17:50 +0200

    small changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,14 +4,14 @@ const bcrypt = require('bcryptjs');
 exports.register = async (req, res) => {
 	try {	
 		
-		const { login, password } = req.body;
-		if (login && password ) {
+		const { login, password, phoneNumber, avatar } = req.body;
+		if (login && typeof login === 'string' && password && typeof password === 'string') {
 			const userWithLogin = await User.findOne({ login });
 			if (userWithLogin) {
 				return res.status(409).send({ message: 'User with this login already exists'});
 			}
 
-			const user = await User.create({ login, password });
+			const user = await User.create({ login, password: await bcrypt.hash(password, 10), phoneNumber, avatar });
 			res.status(201).send({ message: 'User created ' + user.login })
 		} else {
 			res.status(400).send({ message: 'Bad request' });
