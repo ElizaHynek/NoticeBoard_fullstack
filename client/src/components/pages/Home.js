1fcfe2290c90a153caea270c4e493ed56a65cf8f
Eliza Hynek <eliza.hynek@gmail.com>
2023-08-26 14:18:01 +0200

    add spinner and home feature

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,18 +1,62 @@
 import AllAds from "../features/AllAds";
+import Spinner from "../common/Spinner";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { API_URL } from "../../config";
+import { getAllUsers } from "../../redux/usersRedux";
+import { editAd } from "../../redux/adsRedux";
 
 const Home = props => {
 
+  const [pending, setPending] = useState(false);
+  const dispatch = useDispatch();
+  const user = useSelector(getAllUsers);
+
+  useEffect(() => {
+
+    const options = {
+      method: 'GET',
+    };
+
+    const handleUpdate = () => {
+      setPending(true);
+      fetch(`${API_URL}/ads`, options)
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json().then((ads) => {
+            dispatch(editAd(ads));
+            setPending(false);
+          });
+        }
+      });
+    };
+  
+    handleUpdate();
+  
+    if (user) {
+      fetch(`${API_URL}/auth/user/${user.login}`, options)
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json().then((data) => {
+            dispatch(getAllUsers(data));
+          });
+        }
+      });
+    }
+  }, [dispatch, user]);
+
   return (
     <div>
+      {pending && <Spinner />}
       <div className="d-flex justify-content-between">
         <h1>All posts</h1>
         <Link key={props.id} to={"/ad/add"}>
           <Button variant="success">Add advert</Button>
         </Link> 
       </div>
-      <AllAds />
+      {!pending && <AllAds />}
     </div>
   );
 };
--- client/src/redux/initialState.js
+++ client/src/redux/initialState.js
@@ -1,6 +1,48 @@
 const initialState = {
-	ads: [],
-	user: null
+	ads: [
+		{
+			id: '1',
+			title: 'Article title 1',
+			content: 'Main content of the article',
+			price: '3333',
+			publishDate: '02-03-2022',
+			user: 'Martin Smith',
+			location: 'News',
+			image: 'about-us-1690791022499.jpg'
+		},
+	  {
+			id: '2',
+			title: 'Article title 2',
+			content: 'Main content of the article',
+			price: '3333',
+			publishDate: '02-03-2022',
+			user: 'Martin Smith',
+			location: 'News',
+			image: 'coffee-2-1690642819817.png'
+	  },
+		{
+			id: '3',
+			title: 'Article title 3',
+			content: 'Main content of the article',
+			price: '3333',
+			publishDate: '02-03-2022',
+			user: 'Martin Smith',
+			location: 'News',
+			image: 'about-us-1690791022499.jpg'
+			},
+
+		{
+			id: '4',
+			title: 'Article title 4',
+			content: 'Main content of the article',
+			price: '3333',
+			publishDate: '02-03-2022',
+			user: 'Martin Smith',
+			location: 'News',
+			image: 'coffee-2-1690642819817.png'
+			}
+		],
+	users: [null]
   };
   
   export default initialState;
